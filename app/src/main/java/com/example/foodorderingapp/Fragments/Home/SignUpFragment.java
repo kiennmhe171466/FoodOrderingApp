2312c86f57d9e7526a6f5bfacea2874b8784bad8
MINHLD2003 <minhldd8403@gmail.com>
2024-10-26 09:40:35 +0700

    Login Register Feature

--- app/src/main/java/com/example/foodorderingapp/Fragments/Home/SignUpFragment.java
+++ app/src/main/java/com/example/foodorderingapp/Fragments/Home/SignUpFragment.java
@@ -2,6 +2,7 @@ package com.example.foodorderingapp.Fragments.Home;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,7 +25,6 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         binding = FragmentSignUpBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
@@ -37,30 +37,33 @@ public class SignUpFragment extends Fragment {
                     String email = binding.edtEmail.getText().toString();
                     String pass = binding.edtPass.getText().toString();
 
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                User tmp = new User(task.getResult().getUser().getUid(), email, "", name, "01/01/2000", phone);
-                                Cart cart = new Cart(FirebaseDatabase.getInstance().getReference().push().getKey(), 0, 0, task.getResult().getUser().getUid());
-
-                                FirebaseDatabase.getInstance().getReference("Users").child(tmp.getUserId())
-                                        .setValue(tmp).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                if (task.isSuccessful()) {
-                                                    FirebaseDatabase.getInstance().getReference("Carts").child(cart.getCartId()).setValue(cart);
-                                                    Toast.makeText(getContext(), "Create account successfully", Toast.LENGTH_SHORT).show();
-                                                } else {
-                                                    Toast.makeText(getContext(), "Create account unsuccessfully", Toast.LENGTH_SHORT).show();
-                                                }
-                                            }
-                                        });
-                            } else {
-                                createDialog("Email đã tồn tại").show();
-                            }
-                        }
-                    });
+                    // Show loading indicator here
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, pass)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        User tmp = new User(task.getResult().getUser().getUid(), email, "", name, "01/01/2000", phone);
+                                        Cart cart = new Cart(FirebaseDatabase.getInstance().getReference().push().getKey(), 0, 0, task.getResult().getUser().getUid());
+                                        FirebaseDatabase.getInstance().getReference("Users").child(tmp.getUserId())
+                                                .setValue(tmp).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            FirebaseDatabase.getInstance().getReference("Carts").child(cart.getCartId()).setValue(cart);
+                                                            Toast.makeText(getContext(), "Create account successfully", Toast.LENGTH_SHORT).show();
+                                                        } else {
+                                                            Toast.makeText(getContext(), "Create account unsuccessfully", Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                });
+                                    } else {
+                                        String errorMessage = task.getException() != null ? task.getException().getMessage() : "Error occurred";
+                                        createDialog(errorMessage).show();
+                                    }
+                                    // Hide loading indicator here
+                                }
+                            });
                 }
             }
         });
@@ -69,37 +72,29 @@ public class SignUpFragment extends Fragment {
     }
 
     public boolean check() {
-        String phone= binding.edtPhone.getText().toString();
-        String name= binding.edtName.getText().toString();
-        String email= binding.edtEmail.getText().toString();
-        String pass= binding.edtPass.getText().toString();
-        if (phone.isEmpty()|| name.isEmpty()|| email.isEmpty()|| pass.isEmpty()) {
+        String phone = binding.edtPhone.getText().toString();
+        String name = binding.edtName.getText().toString();
+        String email = binding.edtEmail.getText().toString();
+        String pass = binding.edtPass.getText().toString();
+        if (TextUtils.isEmpty(phone) || TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)) {
             createDialog("Điền đầy đủ thông tin").show();
             return false;
-        }
-        else if (!email.matches(String.valueOf(Patterns.EMAIL_ADDRESS))) {
+        } else if (!email.matches(String.valueOf(Patterns.EMAIL_ADDRESS))) {
             createDialog("Email không đúng định dạng").show();
             return false;
-        }
-        else if (!phone.matches("(03|05|07|08|09|01[2689])[0-9]{8}\\b")) {
+        } else if (!phone.matches("(03|05|07|08|09|01[2689])[0-9]{8}\\b")) {
             createDialog("Số điện thoại không hợp lệ").show();
             return false;
         }
+        // Add password strength validation here if needed
         return true;
     }
 
-
     public AlertDialog createDialog(String message) {
-        AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setMessage(message);
         builder.setTitle("Notice");
         builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();

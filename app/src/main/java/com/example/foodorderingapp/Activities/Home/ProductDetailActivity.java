57dd43e98fc39a19be1a114cbfd2a13f47c43fd2
DungBNHE171450 <144671027+DungBNHE171450@users.noreply.github.com>
2024-10-28 17:40:20 +0700

    Product detail UI

--- app/src/main/java/com/example/foodorderingapp/Activities/Home/ProductDetailActivity.java
+++ app/src/main/java/com/example/foodorderingapp/Activities/Home/ProductDetailActivity.java
@@ -1,16 +1,39 @@
 package com.example.foodorderingapp.Activities.Home;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.foodorderingapp.R;
 import com.example.foodorderingapp.databinding.ActivityProfileBinding;
 
 public class ProductDetailActivity extends AppCompatActivity {
+    //private ActivityProductInfoBinding binding;
+    private String productId;
+    private String productName;
+    private int productPrice;
+    private String productDescription;
+    private Double ratingStar;
+    private String productImage1;
+    private String productImage2;
+    private String productImage3;
+    private String productImage4;
+    private String userName;
+    private String userId;
+    private String publisherId;
+    private int sold;
+    private String productType;
+    private int remainAmount;
+    private int ratingAmount;
+    private String state;
+    private boolean own;
+    private Notification notification;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_product_detail);
     }
 }
